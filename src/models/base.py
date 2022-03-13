bfb46ffb019e194dba09490ad26d4b292d2fc260
dang <nguyenhaidang.uit@gmail.com>
2022-03-13 22:35:02 +0100

    add method docs and comment

--- src/models/base.py
+++ src/models/base.py
@@ -6,8 +6,21 @@ class BaseModel(ABC):
 
     @abstractmethod
     def train(self, X: np.ndarray, y: np.ndarray):
+        """
+        Train model.\n
+        Params:
+            X: features
+            y: labels
+        """
         ...
 
     @abstractmethod
-    def predict(self, X: np.ndarray):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict labels for data.\n
+        Params:
+            X: features
+        Returns:
+            Predicted values
+        """
         ...
--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -9,6 +9,7 @@ class LinearRegresson(BaseModel):
         self._beta = None
 
     def train(self, X: np.ndarray, y: np.ndarray):
+        # use Moore-Penrose pseudo-inverse to make sure that matrix always has inverse
         self._beta = np.linalg.pinv(X.dot(X.T)).dot(X.T).dot(y)
 
     def predict(self, X: np.ndarray):
