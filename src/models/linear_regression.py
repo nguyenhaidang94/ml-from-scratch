8d8aae81ebab0a3adf265878039fb088df0faf57
dang <nguyenhaidang.uit@gmail.com>
2022-03-31 22:28:37 +0700

    update linear regression with bias term

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -9,8 +9,11 @@ class LinearRegression(BaseModel):
         self._beta = None
 
     def train(self, X: np.ndarray, y: np.ndarray):
+        # account for the bias
+        X = np.concatenate((X, np.ones((X.shape[0], 1))), axis=1)
         # use Moore-Penrose pseudo-inverse to make sure that matrix always has inverse
         self._beta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)
 
     def predict(self, X: np.ndarray):
+        X = np.concatenate((X, np.ones((X.shape[0], 1))), axis=1)
         return np.dot(X, self._beta)
