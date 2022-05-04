495754cb64f0cc33b626ffa5cf7d4548ba46e5b2
dang <nguyenhaidang.uit@gmail.com>
2022-05-04 22:58:50 +0700

    implement svm

---
+++ src/models/svm.py
@@ -0,0 +1,39 @@
+import cvxopt
+import numpy as np
+
+from .base import BaseModel
+
+
+class SVM(BaseModel):
+
+    def __init__(self):
+        self._w = None
+        self._b = None
+
+    def train(self, X: np.ndarray, y: np.ndarray):
+        n_samples, _ = X.shape
+        # compute inputs for cvxopt solver
+        K = (X * y[:, np.newaxis]).T
+        P = cvxopt.matrix(K.T.dot(K)) # P has shape n*n
+        q = cvxopt.matrix(-1 * np.ones(n_samples)) # q has shape n*1
+        G = cvxopt.matrix(-1 * np.identity(n_samples)) # G is diagonal matrix
+        h = cvxopt.matrix(np.zeros(n_samples))
+        A = cvxopt.matrix(1.0 * y, (1, n_samples))
+        b = cvxopt.matrix(0.0)
+        # solve quadratic programming
+        cvxopt.solvers.options['show_progress'] = False
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+        _lambda = np.ravel(solution['x'])
+        # find support vectors
+        S = np.where(_lambda > 1e-10)[0]
+        self._w = K[:, S].dot(_lambda[S])
+        self._b = np.mean(y[S] - X[S, :].dot(self._w))
+
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Return +1 for positive class and -1 for negative class.
+        """
+        results = np.sign(X.dot(self._w) + self._b)
+        results[results == 0] = 1
+        return results
