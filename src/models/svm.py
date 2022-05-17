4f6d3929425c011df6cf2f4f3503a05401454662
dang <nguyenhaidang.uit@gmail.com>
2022-05-17 21:48:53 +0700

    update svm softmargin constructor

--- src/models/svm.py
+++ src/models/svm.py
@@ -41,18 +41,21 @@ class SVM(BaseModel):
 
 class SVMSoftMargin(BaseModel):
 
-    def __init__(self):
+    def __init__(self, C: float):
+        self.C = C
         self._w = None
         self._b = None
 
-    def train(self, X: np.ndarray, y: np.ndarray, C: float):
+    def train(self, X: np.ndarray, y: np.ndarray):
         n_samples, _ = X.shape
         # compute inputs for cvxopt solver
         K = (X * y[:, np.newaxis]).T
         P = cvxopt.matrix(K.T.dot(K)) # P has shape n*n
         q = cvxopt.matrix(-1 * np.ones(n_samples)) # q has shape n*1
-        G = cvxopt.matrix(np.concatenate((-1*np.identity(n_samples), np.identity(n_samples)), axis=0))
-        h = cvxopt.matrix(np.concatenate((np.zeros(n_samples), C*np.ones(n_samples)), axis=0))
+        G = cvxopt.matrix(np.concatenate((-1*np.identity(n_samples),
+                                          np.identity(n_samples)), axis=0))
+        h = cvxopt.matrix(np.concatenate((np.zeros(n_samples),
+                                          self.C*np.ones(n_samples)), axis=0))
         A = cvxopt.matrix(1.0 * y, (1, n_samples))
         b = cvxopt.matrix(0.0)
         # solve quadratic programming
@@ -60,9 +63,9 @@ class SVMSoftMargin(BaseModel):
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
         _lambda = np.ravel(solution['x'])
         # find support vectors
-        S = np.where((_lambda > 1e-10) & (_lambda <= C))[0]
+        S = np.where((_lambda > 1e-10) & (_lambda <= self.C))[0]
         self._w = K[:, S].dot(_lambda[S])
-        M = np.where((_lambda > 1e-10) & (_lambda < C))[0]
+        M = np.where((_lambda > 1e-10) & (_lambda < self.C))[0]
         self._b = np.mean(y[M] - X[M, :].dot(self._w))
 
     def predict(self, X: np.ndarray) -> np.ndarray:
